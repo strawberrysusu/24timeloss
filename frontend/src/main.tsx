8b2ad6ace6b9149cc7748a7d6cd7e439cdff0d81
jaewonjin-code <jjo3211@naver.com>
2026-04-08 14:49:25 +0900

    refactor(frontend): add frontend-owned style entrypoint

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
-import "../../src/main/resources/static/style.css";
+import "./styles/index.css";
 
 import { App } from "./App";
 
